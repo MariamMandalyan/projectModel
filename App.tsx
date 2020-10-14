a1db9cbbcdf8ae0cbb648b6bb51251d7a2b00092
Edgar <edgar.khachatryan95@mail.ru>
2020-10-14 17:08:09 +0400

    new project

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import {Text } from 'react-native';
+import Components from './src/components'
+const App: React.FC = () => {
+	return (
+    <Components />
+	);
+};
+export default App;
---
+++ src/components.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import {Text } from 'react-native';
+
+const Components: React.FC = () => {
+	return (
+    <Text>Componentsner</Text>
+	);
+};
+export default Components;
